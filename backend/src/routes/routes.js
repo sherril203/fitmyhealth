62dc2394febeedd8ad0a1fdb63751da7d921fa48
sherril203 <sherrilkumar2003@gmail.com>
2026-05-20 23:20:44 +0530

    backend of user and admin completed

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -6,21 +6,25 @@ const data=require('../controllers/data.controllers')
 const user=require('../controllers/user.controllers')
 const admin=require('../controllers/admin.controllers')
 
-
+//user authentication
 router.post('/userRegister',user.UserRegister)
 router.post('/userlogin',user.UserLogin)
 
+//admin authentication
 router.post('/adminRegister',admin.AdminRegister)
 router.post('/adminlogin',admin.AdminLogin)
 
+//feedback
 router.post('/feedpost',feedback.postfeed)
 router.get('/getfeed',feedback.getfeed)
 
+//CRUD in records by user
 router.post('/postrecord',record.postrecord)
 router.get('/getrecord',record.getrecord)
 router.delete('/deleterecord/:id', record.deleterecord);
 router.get('/getrecord/:id', record.getsinglerecord)
 
+//CRUD in data to enter in record by user
 router.post("/postdata/:id",data.postdata)
 router.get("/getdata/:id",data.getdata)
 router.get("/getsingledata/:id",data.getsingledata)
